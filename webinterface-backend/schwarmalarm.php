c425c135972769d7d2fc03b3c2a1d2f26ca0c316
Javan Rasokat <Javan.Rasokat@optisoft.de>
2018-06-14 14:39:02 +0200

    changed to POST request and added HTTP Auth

--- webinterface-backend/schwarmalarm.php
+++ webinterface-backend/schwarmalarm.php
@@ -2,12 +2,31 @@
 
 header('Content-Type: text/html; charset=utf-8');
 
-if (isset($_GET['email']) && isset($_GET['content']) ) {
+/*
+//	TODO: Dieses Passwort auf dem eigenen Webserver ändern:
+$passwort = "meinpasswort123";
 
-	// TODO: Dieses Skript könnte missbraucht werden um unerwünschte Emails vom eigenen Webserver an Fremde zu versenden.
-	// Daher Pfad dieses Skriptes geheim halten oder Zugang schützen.
-	$empfaenger = filter_var($_GET['email'], FILTER_VALIDATE_EMAIL);
-	$text = htmlspecialchars($_GET['content']);
+// Dieses Skript könnte missbraucht werden um unerwünschte Emails vom eigenen Webserver an Fremde zu versenden.
+// Daher den Pfad dieses Skriptes geheim halten oder den Zugang mit einer "HTTP Authentication" schützen.
+$valid_passwords = array ("schwarmalarm" => $passwort);
+$valid_users = array_keys($valid_passwords);
+
+$user = $_SERVER['PHP_AUTH_USER'];
+$pass = $_SERVER['PHP_AUTH_PW'];
+
+$validated = (in_array($user, $valid_users)) && ($pass == $valid_passwords[$user]);
+
+if (!$validated) {
+  header('WWW-Authenticate: Basic realm="Private"');
+  header('HTTP/1.0 401 Unauthorized');
+  die ("Diese Seite ist geschützt mit einer HTTP Authentication.");
+}
+*/
+
+if (isset($_REQUEST['email']) && isset($_REQUEST['content']) ) {
+
+	$empfaenger = filter_var($_REQUEST['email'], FILTER_VALIDATE_EMAIL);
+	$text = htmlspecialchars($_REQUEST['content']);
 	$betreff = "Schwarmalarm";
 	$headers = "MIME-Version: 1.0" . "\r\n";
 	$headers .= "Content-type: text/html; charset=UTF-8" . "\r\n";
