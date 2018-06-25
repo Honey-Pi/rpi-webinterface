42043bd867f62ef719bdb75f4682be38c48e70b7
JavanXD <mail@javan.de>
2018-06-25 12:19:22 +0200

    fixed typo and reference_unit

--- dist/styles.js
+++ dist/styles.js
@@ -539,7 +539,7 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Projects\HoneyPi\rpi-webinterface\src\styles.css */"./src/styles.css");
+module.exports = __webpack_require__(/*! C:\Users\javan\Desktop\HoneyPi\rpi-webinterface\src\styles.css */"./src/styles.css");
 
 
 /***/ })
