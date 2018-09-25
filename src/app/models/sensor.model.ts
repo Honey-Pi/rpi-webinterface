038b1ece1595dddb755e8e5cb6ae1c03229802b4
JavanXD <mail@javan.de>
2018-09-25 19:42:47 +0200

    added support for DHT11/DHT22

--- src/app/models/sensor.model.ts
+++ src/app/models/sensor.model.ts
@@ -5,6 +5,7 @@ export class Sensor {
   Type 0: DS18b20
   Type 1: BME680
   Type 2: HX711
+  Type 3: DHT11/DHT22
    */
 
   type: number;
@@ -17,10 +18,13 @@ export class Sensor {
   pin_dt: number;
   pin_sck: number;
   // BME680 spezifisch:
-  ts_field_temperature: string;
-  ts_field_humidity: string;
   ts_field_air_pressure: string;
   ts_field_air_quality: string;
+  // BME680 UND DHT11:
+  ts_field_temperature: string;
+  ts_field_humidity: string;
   // DS18b20 spezifisch:
   device_id: string;
+  // DHT11
+  pin: number;
 }
