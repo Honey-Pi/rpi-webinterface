266c144c7df4a564b367d8d60bf7a72f6c42b944
JavanXD <mail@javan.de>
2018-12-11 20:17:25 +0100

    fix wifi naming problem

--- backend/shell-scripts/change_router_ssidpw.sh
+++ backend/shell-scripts/change_router_ssidpw.sh
@@ -3,5 +3,5 @@
 ssid=$1;
 pw=$2;
 
-sudo sed -i 's/^psk=.*/psk="'$pw'"/' /etc/wpa_supplicant/wpa_supplicant.conf;
-sudo sed -i 's/^ssid=.*/ssid="'$ssid'"/' /etc/wpa_supplicant/wpa_supplicant.conf;
+sudo sed -i "s/^psk=.*/psk=\"$pw\"/" /etc/wpa_supplicant/wpa_supplicant.conf;
+sudo sed -i "s/^ssid=.*/ssid=\"$ssid\"/" /etc/wpa_supplicant/wpa_supplicant.conf;
