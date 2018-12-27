e2393a30e4b27a4b7bf94e9bffe6a9de3f8f6ebc
JavanXD <mail@javan.de>
2018-12-27 17:33:02 +0100

    texts

--- src/app/components/log/log.component.ts
+++ src/app/components/log/log.component.ts
@@ -34,33 +34,6 @@ export class LogComponent implements OnInit {
       }, (err: any) => {console.log(err.status); console.log(err);});
   }
 
-  public downloadSettingsFile() {
-    this.appService.getSettings().subscribe(data => this.downloadFile(data)),//console.log(data),
-      error => console.log('Error downloading the file.'),
-      () => console.info('OK');
-  }
-
-  private downloadFile(data: Settings) {
-    let reader = new FileReader();
-    const blob = new Blob([data], { type: 'application/octet-stream'});
-    let url = window.URL.createObjectURL(new Blob([data], {type: 'application/json'}));
-    /*let link=document.createElement('a');
-    link.href=window.URL.createObjectURL(blob);
-    link.download="settings.json";
-    link.click();
-    */
-    /*let headers = new Headers();
-    headers.append('responseType', 'arraybuffer');
-    reader.readAsDataURL(blob);
-    reader.onloadend = (e) => {
-      window.open(reader.result);
-    }*/
-    /*
-    const blob = new Blob([JSON.stringify(data)], { type: 'application/json'});
-    window.open(window.URL.createObjectURL(blob), "_blank");
-    */
-  }
-
   public onFileChanged(event) {
     let file: File = event.target.files[0];
     console.log(file);
