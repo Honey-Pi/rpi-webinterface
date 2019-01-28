88e00841209abb2e5b3832d9f8607d399d3e3db5
Javan R <mail@javan.de>
2019-01-28 19:01:49 +0100

    fix space bug in AP

--- backend/shell-scripts/change_honeypi_ssidpw.sh
+++ backend/shell-scripts/change_honeypi_ssidpw.sh
@@ -5,5 +5,5 @@
 ssid=$1;
 pw=$2;
 
-sudo sed -i 's/^wpa_passphrase=.*/wpa_passphrase='$pw'/' /etc/hostapd/hostapd.conf;
-sudo sed -i 's/^ssid=.*/ssid='$ssid'/' /etc/hostapd/hostapd.conf;
+sudo sed -i "s/^wpa_passphrase=.*/wpa_passphrase=\"$pw\"/" /etc/hostapd/hostapd.conf;
+sudo sed -i "s/^ssid=.*/ssid=\"$ssid\"/" /etc/hostapd/hostapd.conf;
