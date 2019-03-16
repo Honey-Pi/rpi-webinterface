0b0db7ad380ac889116b4cc1e0c091d3feb9248f
Javan R <mail@javan.de>
2019-03-16 21:18:05 +0100

    detect browser language

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {TranslateService} from "@ngx-translate/core";
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-navbar',
@@ -10,10 +10,12 @@ export class NavbarComponent implements OnInit {
 
   constructor(translate: TranslateService) {
     // this language will be used as a fallback when a translation isn't found in the current language
-    translate.setDefaultLang('de');
+    translate.setDefaultLang('en');
+
+    console.log('Browser language is: ' + translate.getBrowserLang());
 
     // the lang to use, if the lang isn't available, it will use the current loader to get them
-    translate.use('en');
+    translate.use(translate.getBrowserLang());
   }
 
   ngOnInit() {
