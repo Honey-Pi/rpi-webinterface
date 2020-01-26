b6b77490985a185852f9d6162783686876a4579d
Javan R <mail@javan.de>
2020-01-26 15:47:05 +0100

    Update change_honeypi_ssidpw.sh

--- backend/shell-scripts/change_honeypi_ssidpw.sh
+++ backend/shell-scripts/change_honeypi_ssidpw.sh
@@ -3,7 +3,5 @@
 ssid=$1;
 pw=$2;
 
-# updates ssid and pw for honeypi
-
 sudo sed -i "s/^wpa_passphrase=.*/wpa_passphrase="$pw"/" /etc/hostapd/hostapd.conf;
 sudo sed -i "s/^ssid=.*/ssid="$ssid"/" /etc/hostapd/hostapd.conf;
