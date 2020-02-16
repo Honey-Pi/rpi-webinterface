6fe039f2fba8df5a85c387b8b0ef553ec8e1e3da
Javan R <mail@javan.de>
2020-02-16 09:42:34 +0100

    prevent ssl errors

--- backend/update.php
+++ backend/update.php
@@ -36,19 +36,22 @@
 
     function getVersionInfo() {
         $obj = new \stdClass();
-        $context = stream_context_create(
-                        array(
-                            "http" => array(
-                                "header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
-                            )
-                        )
-                    );
+        $arrContextOptions=stream_context_create(array(
+            // disable ssl cert
+            "ssl"=>array(
+                "verify_peer"=>false,
+                "verify_peer_name"=>false,
+            ),
+            "http" => array(
+                "header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
+            )
+        ));
         try {
             $urlScripts = "https://api.github.com/repos/Honey-Pi/rpi-scripts/releases/latest";
-            $contentScripts = file_get_contents($urlScripts, false, $context);
+            $contentScripts = @file_get_contents($urlScripts, false, $arrContextOptions);
 
             $urlWebinterface = "https://api.github.com/repos/Honey-Pi/rpi-webinterface/releases/latest";
-            $contentWebinterface = file_get_contents($urlWebinterface, false, $context);
+            $contentWebinterface = @file_get_contents($urlWebinterface, false, $arrContextOptions);
 
             // get future updates
             $obj->scripts = json_decode($contentScripts);
