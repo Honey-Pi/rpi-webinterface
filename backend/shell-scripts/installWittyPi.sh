f1f90901c92de8c4caf63c85269638d371eb7116
Javan R <mail@javan.de>
2020-02-25 17:46:13 +0100

    Update installWittyPi.sh

--- backend/shell-scripts/installWittyPi.sh
+++ backend/shell-scripts/installWittyPi.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# install WittyPi into home directory
 cd /home/pi/
 
 if [ -z "$1" ] ; then
@@ -7,9 +8,11 @@ if [ -z "$1" ] ; then
 else
 	wittyPi=$1
 
-    get https://project-downloads.drogon.net/wiringpi-latest.deb
+    # update to latest wiringpi version (required for raspbian buster)
+    wget https://project-downloads.drogon.net/wiringpi-latest.deb
     sudo dpkg -i wiringpi-latest.deb
 
+    # Run the installer provided from WittyPi
     if [ $wittyPi -eq 2 ] ; then
         sudo sh /var/www/html/backend/installWittyPi2.sh
     elif [ $wittyPi -eq 3 ] ; then
