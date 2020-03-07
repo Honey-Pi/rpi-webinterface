ce78d12c506fcce4146649f69139d180a4f1dcff
Javan R <mail@javan.de>
2020-03-08 00:09:29 +0100

    don't require restart after wifi change

--- backend/shell-scripts/change_router_ssidpw.sh
+++ backend/shell-scripts/change_router_ssidpw.sh
@@ -5,3 +5,6 @@ pw=$2;
 
 sudo sed -i "s/psk=.*/psk=\"$pw\"/" /etc/wpa_supplicant/wpa_supplicant.conf;
 sudo sed -i "s/ssid=.*/ssid=\"$ssid\"/" /etc/wpa_supplicant/wpa_supplicant.conf;
+
+# connect to wifi without reboot
+sudo wpa_cli -i wlan0 reconfigure
