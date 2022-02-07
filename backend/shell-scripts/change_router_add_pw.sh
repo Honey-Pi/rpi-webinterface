26b35cc0a366a5891e39ac5f80826849b1cdeb4d
Javan Rasokat <mail@javan.de>
2022-02-07 15:21:23 +0100

    removed unused code

--- backend/shell-scripts/change_router_add_pw.sh
+++ backend/shell-scripts/change_router_add_pw.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
 
-#sudo sed -i "s/#psk=.*/psk=\"$pw\"/" /etc/wpa_supplicant/wpa_supplicant.conf;
-
 pw=$1;
 sudo sed -i "/#nopsk/c\psk=\"$pw\"" /etc/wpa_supplicant/wpa_supplicant.conf;
--- backend/shell-scripts/change_router_remove_pw.sh
+++ backend/shell-scripts/change_router_remove_pw.sh
@@ -1,6 +1,3 @@
 #! /bin/bash
 
-#pw=$1;
-#sudo sed -i "s/psk=.*/#psk=\"$pw\"/" /etc/wpa_supplicant/wpa_supplicant.conf;
-
 sudo sed -i '/psk/c\#nopsk' /etc/wpa_supplicant/wpa_supplicant.conf;
